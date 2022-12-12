461461eb43c8b505dff6c79739bafa66a4f503e2
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2022-12-12 15:58:44 +0000

    Add files via upload

---
+++ Website/websi.php
@@ -0,0 +1,543 @@
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Be Counted</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+  </head>
+  <style>
+    html,
+    body {
+      height: 100%;
+    }
+
+    body {
+      margin: 0px;
+      padding: 0px;
+      background: white;
+      font-family: "Didact Gothic", sans-serif;
+      font-size: 12pt;
+      font-weight: 200;
+      color: #ffff;
+    }
+
+    h1,
+    h2,
+    h3 {
+      margin: 0;
+      padding: 0;
+    }
+
+    p,
+    ol,
+    ul {
+      margin-top: 0;
+    }
+
+    ol,
+    ul {
+      padding: 0;
+      list-style: none;
+    }
+
+    p {
+      line-height: 180%;
+    }
+
+    .container {
+      overflow: hidden;
+      margin: 0em auto;
+      width: 1200px;
+    }
+
+    ul.actions li {
+      display: inline-block;
+      padding: 0em 0.5em;
+    }
+
+    .title {
+      margin-bottom: 3em;
+    }
+
+    .title h2 {
+      font-size: 2.7em;
+    }
+
+    .title .byline {
+      font-size: 1.3em;
+      color: rgba(255, 255, 255, 0.6);
+    }
+
+    #wrapper {
+      position: relative;
+      padding: 5em 0em 7em 0em;
+      background: #fff;
+    }
+
+    #header-wrapper {
+      position: relative;
+      padding: 5em 0em 7em 0em;
+      background: #111111 url(pic21.jpg) no-repeat center;
+      background-size: cover;
+    }
+
+    #header {
+      position: relative;
+      padding: 5em 0em;
+    }
+
+    #logo h1 {
+      font-size: 2.6em;
+      color: #79c255;
+    }
+
+    #menu {
+      position: absolute;
+      top: 4.5em;
+      right: 0;
+    }
+
+    #menu ul {
+      display: inline-block;
+    }
+
+    #menu li {
+      display: block;
+      float: left;
+      text-align: center;
+    }
+
+    #menu li a,
+    #menu li span {
+      padding: 1em 1.5em;
+      letter-spacing: 1px;
+      text-decoration: none;
+      text-transform: uppercase;
+      font-size: 0.8em;
+      color: rgba(255, 255, 255, 0.8);
+    }
+
+    #menu .active a {
+      background: #2651a8;
+      border-radius: 6px;
+      color: rgba(255, 255, 255, 1);
+    }
+
+    #banner {
+      padding-top: 10em;
+      text-align: center;
+    }
+
+    #banner .title h2 {
+      font-size: 4em;
+    }
+
+    #banner .title .byline {
+      font-size: 2em;
+    }
+
+    #banner .button {
+      margin-top: 2em;
+      padding: 1.4em 3em;
+      border-radius: 6px;
+      font-weight: 700;
+      font-size: 1em;
+    }
+
+    .banner {
+      margin-bottom: 5em;
+    }
+
+    #footer {
+    }
+
+    #copyright {
+      overflow: hidden;
+      padding: 5em 0em;
+      border-top: 1px solid rgba(0, 0, 0, 0.08);
+    }
+
+    #copyright p {
+      text-align: center;
+      font-size: 1em;
+      color: rgba(255, 255, 255, 0.3);
+    }
+
+    #copyright a {
+      text-decoration: none;
+      color: rgba(255, 255, 255, 0.6);
+    }
+
+    #three-column {
+      text-align: cSenter;
+      color: rgba(0, 0, 0, 0.6);
+    }
+
+    #three-column .fa {
+      display: block;
+      padding: 1em 0em;
+      color: rgba(0, 0, 0, 1);
+      font-size: 2em;
+    }
+
+    #three-column .title h2 {
+      font-weight: bold;
+      color: rgba(0, 0, 0, 0.8);
+    }
+
+    .wrapper {
+      margin-top: 5%;
+    }
+
+    .wrapper h1 {
+      font-size: 30px;
+      margin-bottom: 60px;
+      text-align: center;
+    }
+
+    .team {
+      display: flex;
+      justify-content: center;
+      width: auto;
+      text-align: center;
+      flex-wrap: wrap;
+    }
+
+    .team .team_member {
+      background: rgb(98, 85, 85);
+      margin: 5px;
+      margin-bottom: 50px;
+      width: 300px;
+      height: 200px;
+      padding: 20px;
+      line-height: 20px;
+      color: #8e8b8b;
+      position: relative;
+    }
+
+    .team .team_member h3 {
+      color: red;
+      font-size: 26px;
+      margin-top: 50px;
+    }
+
+    .team .team_member p.role {
+      color: #ccc;
+      margin: 12px 0;
+      font-size: 12px;
+      text-transform: uppercase;
+    }
+
+    .team .team_member .team_img {
+      position: absolute;
+      top: -50px;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background: #fff;
+      border: 3px solid red;
+    }
+
+    .team .team_member .team_img img {
+      width: 100px;
+      height: 100px;
+      padding: 5px;
+    }
+    .num1 {
+      width: 100px;
+      height: 100px;
+      background-color: #fff;
+      border: white solid 1px;
+      margin-left: 210px;
+    }
+
+    .num2 {
+      width: 100px;
+      height: 100px;
+      background-color: #fff;
+      border: white solid 1px;
+      margin-left: 560px;
+      position: absolute;
+      top: 221.7%;
+    }
+
+    .num3 {
+      width: 100px;
+      height: 100px;
+      background-color: #fff;
+      border: white solid 1px;
+      margin-left: 880px;
+      position: absolute;
+      top: 221.7%;
+    }
+
+
+    .odk:hover{
+        background-color: red;
+        transition: .3s;
+      }
+
+
+  </style>
+
+  <body>
+    <div id="header-wrapper">
+      <div id="header" class="container">
+        <div id="menu">
+          <ul>
+            <li class="active">
+              <a href="list.php" accesskey="1" title="">Check Status</a>
+            </li>
+            <li><a href="#odk" accesskey="2" title="">Our Services</a></li>
+            <li><a href="#odk" accesskey="3" title="">About Us</a></li>
+            <li><a href="#odk" accesskey="5" title="">Statistics</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="banner" class="container">
+        <div class="title">
+          <h2>2022 CENSUS IS BACK AGAIN BE COUNTED</h2>
+          <span class="byline">AS A GOOD CITIZEN OF GHANA</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <h1 style="color: black">OUR OFFICERS</h1>
+      <div class="team">
+        <div class="team_member">
+          <div class="team_img">
+            <img src="team1.jpg" alt="Team_image" style="border-radius: 50px" />
+          </div>
+          <h3>Fransica Amos</h3>
+          <p class="role">Snr Supervisor</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat
+          </p>
+        </div>
+        <div class="team_member">
+          <div class="team_img">
+            <img src="team1.jpg" alt="Team_image" style="border-radius: 50px" />
+          </div>
+          <h3>Fransica Amos</h3>
+          <p class="role">Supervisor</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat
+          </p>
+        </div>
+        <div class="team_member">
+          <div class="team_img">
+            <img src="team1.jpg" alt="Team_image" style="border-radius: 50px" />
+          </div>
+          <h3>Fransica Amos</h3>
+          <p class="role">Analysics</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat
+            lorem
+          </p>
+        </div>
+      </div>
+
+      <div class="knowus" style="text-align: center; color: #111111">
+        <h3
+          style="
+            background-color: rgba(0, 128, 0, 0);
+            border: 1px solid rgb(254, 4, 4);
+            text-decoration: transparent;
+            text-align: center;
+            padding: 20px;
+            width: 195px;
+            border-radius: 25px;
+            margin-left: 40%;
+          "
+        >
+          Get to Know Us
+        </h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+          nemo provident. Temporibus enim even iet molestias, eum animi
+          veritatis in. Deserunt! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Illo quaerat quasi aut. Necessitatibus possimus id
+          voluptatem. Aspernatur sunt natus doloribus est enim quis tempore,
+          dolor porro aut? Dolor necessitatibus veniam maxime quod nulla
+          nesciunt quidem est earum animi, quia consequuntur saepe Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Consequatur veniam,
+          consequuntur itaque voluptatibus maiores hic facere alias odio!
+          Laborum reprehenderit autem quos consequuntur quibusdam nemo
+          doloremque nulla consectetur quae ipsa. atque ipsum minus. Minus odit
+          numquam dicta architecto distinctio?
+        </p>
+      </div>
+
+      <br /><br />
+      <p
+        style="
+          background-color: rgba(0, 128, 0, 0);
+          text-decoration: transparent;
+          text-align: center;
+          color: red;
+          padding: 20px;
+          width: 195px;
+          position: absolute;
+          transform: translate(-50%, -50%);
+          left: 50%;
+        "
+      >
+        Final Breakdown
+      </p>
+
+      <br />
+      <br />
+      <br />
+      <!-- figures -->
+
+      <div class="odk"
+        style="
+          color: #111111;
+          background-color: blue;
+          width: 80px;
+          height: 90px;
+          margin-left: 200px;text-align:center ; font-size:35px; color:white;
+        "
+      >        
+
+               <?php
+
+
+  $con = mysqli_connect("localhost","root","","population_db");
+  
+    
+    $sql = "SELECT * from people WHERE gender = 'female'";
+  
+    if ($result = mysqli_query($con, $sql)) {
+  
+    $rowcount = mysqli_num_rows( $result );
+    
+    
+    printf(" %d\n", $rowcount);
+  }
+  
+  
+  mysqli_close($con);
+  
+  ?>
+
+
+
+
+  <p style="font-size: 15px";>Female(s)</p>
+
+       </div>
+      <div
+        style="
+          color: #111111;
+          background-color: blue;
+          width: 80px;
+          height: 90px;
+          margin-left: 600px;
+          position: absolute;
+          top: 242%;text-align:center ; font-size:35px; color:white;
+        "
+      >
+           
+             <?php
+
+
+  $con = mysqli_connect("localhost","root","","population_db");
+  
+    
+    $sql = "SELECT * from people WHERE gender = 'male'";
+  
+    if ($result = mysqli_query($con, $sql)) {
+  
+    $rowcount = mysqli_num_rows( $result );
+    
+    
+    printf(" %d\n", $rowcount);
+  }
+  
+  
+  mysqli_close($con);
+  
+  ?>
+
+
+
+
+
+
+
+         <p style="font-size: 15px";>Male(s)</p>
+
+
+
+   </div>
+      <div
+        style="
+          color: #111111;
+          background-color: blue;
+          width: 80px;
+          height: 90px;
+          margin-left: 950px;
+          position: absolute;
+          top: 242%; text-align:center ; font-size:35px; color:white; "
+      >
+           
+            
+          <?php
+
+
+  $con = mysqli_connect("localhost","root","","population_db");
+  
+    
+    $sql = "SELECT * from people";
+  
+    if ($result = mysqli_query($con, $sql)) {
+  
+    $rowcount = mysqli_num_rows( $result );
+    
+    
+    printf(" %d\n", $rowcount);
+  }
+  
+  
+  mysqli_close($con);
+  
+  ?>
+
+             
+
+
+
+
+       
+         <p style="font-size: 15px";>Total</p>
+  </div>
+
+      <br />
+
+      
+
+    <div class="nu" style="background-color: rgb(13, 107, 150)">
+      <div id="copyright" class="container">
+        <p style="color: 3"></p>
+        <p style="color: white">
+          Contact us: +233 (0) xxxxxxxxxx || +233 (0) xxxxxxxxxx ||+233 (0)
+          xxxxxxxxxx </p> <br />
+        </p>
+        <p style="color: white">
+          Locate us: Accra. GPS: GA- XXXX-XXXX </p>
+
+          
+          
+          
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
