82df9710ae45abc098915735e76c6d33daaabd32
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2022-12-13 08:21:56 +0000

    Update list.php

--- Website/list.php
+++ Website/list.php
@@ -96,7 +96,6 @@
           </div>
         </div>
       </div>
-
       <a
         href="login.php"
         style="
@@ -110,7 +109,6 @@
         >Back</a
       >
     </div>
-
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
   </body>
